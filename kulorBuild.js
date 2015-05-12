fe1e5301dfd360efdf3f2d9e3336a9c761bc94f3
chen8114@sina.com <chen8114@sina.com>
2015-05-12 12:15:03 +0800

    check file exists

--- kulorBuild.js
+++ kulorBuild.js
@@ -1,11 +1,15 @@
 module.exports = function( kulor , grunt , tool , log ) {
-    var _dir    = kulor.cacheDir + "kulor-git/";
-    grunt.file.copy( _dir + "Gruntfile.js" , "Gruntfile.js"  );
-    grunt.file.copy( _dir + "package.json" , "package.json"  );
-    if( grunt.file.isDir( kulor.cwd + "src/" ) ){
-        log( "folder src exsits..." );
+    var _dir    = kulor.cacheDir + "kulor/";
+    if( grunt.file.exists( _dir + "Gruntfile.js" ) ){
+        grunt.file.copy( _dir + "Gruntfile.js" , "./Gruntfile.js"  );
+    }
+    if( grunt.file.exists( _dir + "package.json" ) ){
+        grunt.file.copy( _dir + "package.json" , "./package.json"  );    
+    }
+    if( grunt.file.isDir( "src" ) ){
+        log( "folder src exists..." );
     } else {
-        tool.file.copy( _dir + "src/js" , "src/js" );
+        tool.file.copy( _dir + "src/js" , "./src/js" );
         tool.file.copy( _dir + "grunt" , "./grunt" );
         log( "kulor init success..." );
     }
